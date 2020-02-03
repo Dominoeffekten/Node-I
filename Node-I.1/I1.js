649b989bb321eff4225e35b8dfeec44c7b9e6925
Dominoeffekten <rikkedomino@gmail.com>
2020-02-03 12:32:09 +0100

    ændringer tilføjet

--- Node-I.1/I1.js
+++ Node-I.1/I1.js
@@ -8,25 +8,27 @@ let email = readline.question("Enter your email: ");
 
 let fs = require('fs');
 let filename = 'userinfo.json';
-var members = [];
 
-let new_lines = {
+var members = {
+    member: []
+};
+var data = {
     UserID: `${userID}`, 
     email: `${email}`
 };
-var jsonText = JSON.stringify(new_lines);
-members.push(jsonText + ",");
+
+members.member.push(data);
+
+var json = JSON.stringify(members);
 
 
 /* 
 * Opgaven blev først lavet en tekst fil, men for at gøre det nemmere blev det lavet om til en JSON. 
 * Her er muligheden for at tilgå informationen nemmere
 */
-
-
 fs.readFile(filename, (err, data) => { //læser data og printer det ud
     if (err) {
-        fs.writeFile(filename, members, (err) => { //laver en ny fil
+        fs.writeFile(filename, json, (err) => { //laver en ny fil
             if (err) {
                 throw err;
             }
@@ -34,11 +36,12 @@ fs.readFile(filename, (err, data) => { //læser data og printer det ud
         });   
 
     } else {
-        if(members.includes(`${userID}`) == true){ //undersøger om værdien er i arrayet
+        if(data.includes(`${userID}`) == true){ //undersøger om værdien er i arrayet
             console.log(`${email}`);
             console.log("You have a user here. The email is: " + `${email}`);
         } else{
-            fs.appendFile(filename, members, (err) => {  //tilføjer information i filen
+            var pushIt = members.member.push(data);
+            fs.appendFile(filename, pushIt, (err) => {  //tilføjer information i filen
                 if(err) {
                     throw err;
                 }
